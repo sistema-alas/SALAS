15e46ddf1c9ad6c848830eb912af8cad67060847
Alvaro Dos Santos Ferreira <alvarodossantosferreira663@gmail.com>
2025-06-26 18:05:54 -0300

    Ajustando as cores do site e a tela de recuperar senha

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rober\OneDrive\Documentos\curso.PI\projeto.pi\SALAS\.venv
+set VIRTUAL_ENV=C:\Users\Alvaro Santos\Documents\GitHub\SALAS(md3)\SALAS\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
